5388b6dbb027a9986e81a9238c9de4066c159605
ilyas cherfi <ilyascherfi91@gmail.com>
2025-07-24 18:40:38 +0200

    initiate service-client component

---
+++ front/src/app/service-client/service-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-client',
+  imports: [],
+  templateUrl: './service-client.component.html',
+  styleUrl: './service-client.component.scss'
+})
+export class ServiceClientComponent {
+
+}
