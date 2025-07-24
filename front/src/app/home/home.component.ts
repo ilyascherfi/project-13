994b0e5b1e71ae6d0cfce2c0b2478e4b63075e9b
ilyas cherfi <ilyascherfi91@gmail.com>
2025-07-24 18:25:24 +0200

    add routes for the service

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  router = inject(Router);
+  chatService = inject(ChatService); // Injection de service simplifiée
+  newName = signal('');
 
+  updateName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.newName.set(input.value);
+  }
+
+  newChat() {
+    this.chatService.newChat(this.newName()).subscribe((response: string) => {
+      this.router.navigate(['/chat', response]);
+    });
+  }
 }
