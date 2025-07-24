b29259327b3041d8933b77cd1728e63ed65727c5
ilyas cherfi <ilyascherfi91@gmail.com>
2025-07-24 18:39:03 +0200

    implement chat component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatComponent,
+    title: 'Chat'
+  },
   {
     path: '',
     component: HomeComponent,
