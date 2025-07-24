a03214f4ccec1c8db8fd4f1f89e64ec571320789
ilyas cherfi <ilyascherfi91@gmail.com>
2025-07-24 19:01:47 +0200

    implement service client component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
+import { ServiceClientComponent } from './service-client/service-client.component';
 
 export const routes: Routes = [
   {
@@ -13,4 +14,9 @@ export const routes: Routes = [
     component: HomeComponent,
     title: 'Chat'
   },
+  {
+    path: 'chat/service-client',
+    component: ServiceClientComponent,
+    title: 'Chat'
+  }
 ];
